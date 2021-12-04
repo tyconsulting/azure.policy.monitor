053dfda6038f45426f02c669aab2eefb06062050
Tao Yang <tao.yang@tyconsulting.com.au>
2021-12-04 23:34:40 +1100

    .

--- function/PolicyMonitor/__init__.py
+++ function/PolicyMonitor/__init__.py
@@ -10,7 +10,9 @@ import azure.functions as func
 import requests
 
 
-def build_la_api_signature(customer_id, shared_key, date, content_length, method, content_type, resource):
+def build_la_api_signature(
+    customer_id, shared_key, date, content_length, method, content_type, resource
+):
     x_headers = 'x-ms-date:' + date
     string_to_hash = method + "\n" + \
         str(content_length) + "\n" + content_type + \
@@ -24,15 +26,20 @@ def build_la_api_signature(customer_id, shared_key, date, content_length, method
 
 
 def post_la_data(customer_id, shared_key, body, log_type):
-    method = 'POST'
-    content_type = 'application/json'
-    resource = '/api/logs'
-    rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+    method = "POST"
+    content_type = "application/json"
+    resource = "/api/logs"
+    rfc1123date = datetime.datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
     content_length = len(body)
     signature = build_la_api_signature(
         customer_id, shared_key, rfc1123date, content_length, method, content_type, resource)
-    uri = 'https://' + customer_id + '.ods.opinsights.azure.com' + \
-        resource + '?api-version=2016-04-01'
+    uri = (
+        "https://"
+        + customer_id
+        + ".ods.opinsights.azure.com"
+        + resource
+        + "?api-version=2016-04-01"
+    )
 
     headers = {
         'content-type': content_type,
@@ -43,21 +50,21 @@ def post_la_data(customer_id, shared_key, body, log_type):
 
     response = requests.post(uri, data=body, headers=headers)
     if (response.status_code >= 200 and response.status_code <= 299):
-        logging.info('Accepted')
+        logging.info("Accepted")
     else:
         logging.info("Response code: {}".format(response.status_code))
 
 
 def main(event: func.EventGridEvent):
     result = json.dumps({
-        'id': event.id,
-        'data': event.get_json(),
-        'topic': event.topic,
-        'subject': event.subject,
-        'event_type': event.event_type,
+        "id": event.id,
+        "data": event.get_json(),
+        "topic": event.topic,
+        "subject": event.subject,
+        "event_type": event.event_type,
     })
-    log_type = 'PolicyInsights'
-    customer_id = os.environ['WORKSPACE_ID']
-    shared_key = os.environ['WORKSPACE_KEY']
-    logging.info('Python EventGrid trigger processed an event: %s', result)
+    log_type = "PolicyInsights"
+    customer_id = os.environ["WORKSPACE_ID"]
+    shared_key = os.environ["WORKSPACE_KEY"]
+    logging.info("Python EventGrid trigger processed an event: %s", result)
     post_la_data(customer_id, shared_key, result, log_type)
