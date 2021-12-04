60f4f6807fed0f4332cdec0462596aca99fc633f
Tao Yang <tao.yang@tyconsulting.com.au>
2021-12-04 23:44:19 +1100

    .

--- function/PolicyMonitor/__init__.py
+++ function/PolicyMonitor/__init__.py
@@ -19,8 +19,9 @@ def build_la_api_signature(
         "\n" + x_headers + "\n" + resource
     bytes_to_hash = bytes(string_to_hash, encoding="utf-8")
     decoded_key = base64.b64decode(shared_key)
-    encoded_hash = base64.b64encode(hmac.new(
-        decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest()).decode()
+    encoded_hash = base64.b64encode(
+        hmac.new(decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest()
+    ).decode()
     authorization = "SharedKey {}:{}".format(customer_id, encoded_hash)
     return authorization
 
@@ -32,7 +33,14 @@ def post_la_data(customer_id, shared_key, body, log_type):
     rfc1123date = datetime.datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
     content_length = len(body)
     signature = build_la_api_signature(
-        customer_id, shared_key, rfc1123date, content_length, method, content_type, resource)
+        customer_id,
+        shared_key,
+        rfc1123date,
+        content_length,
+        method,
+        content_type,
+        resource,
+    )
     uri = (
         "https://"
         + customer_id
@@ -42,10 +50,10 @@ def post_la_data(customer_id, shared_key, body, log_type):
     )
 
     headers = {
-        'content-type': content_type,
-        'Authorization': signature,
-        'Log-Type': log_type,
-        'x-ms-date': rfc1123date
+        "content-type": content_type,
+        "Authorization": signature,
+        "Log-Type": log_type,
+        "x-ms-date": rfc1123date
     }
 
     response = requests.post(uri, data=body, headers=headers)
